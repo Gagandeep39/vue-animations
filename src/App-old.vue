5ccb475e500b7be2e0db04afafe839bbbfcde00e
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-16 16:51:30 +0000

    Moved css animation code to -old file

---
+++ src/App-old.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="container">
+    <div class="block" :class="{ animate: animatedBlock }"></div>
+    <button @click="animateBlock">
+      Animate
+    </button>
+  </div>
+  <div class="container">
+    <transition
+      name="para"
+      @before-enter="beforeEnterTransition"
+      @before-leave="beforeLeaveTransition"
+    >
+      <p v-if="paragraphIsVisible">Sometie Visible...</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
+
+  <!-- Transition with multiple elemnts -->
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
+
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
+    <p>This is a test dialog!</p>
+    <button @click="hideDialog">Close it!</button>
+  </base-modal>
+  <div class="container">
+    <button @click="showDialog">Show Dialog</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialogIsVisible: false,
+      animatedBlock: false,
+      paragraphIsVisible: false,
+      usersAreVisible: false,
+    };
+  },
+  methods: {
+    toggleParagraph() {
+      this.paragraphIsVisible = !this.paragraphIsVisible;
+    },
+    animateBlock() {
+      this.animatedBlock = !this.animatedBlock;
+    },
+    showDialog() {
+      this.dialogIsVisible = true;
+    },
+    hideDialog() {
+      this.dialogIsVisible = false;
+    },
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    },
+    beforeEnterTransition() {
+      // Executed on enter transtion
+      // We also get the element as param on which transition is performed
+      console.log('beforeEnterTransition');
+    },
+    beforeLeaveTransition() {
+      // Executed on leave transtion
+      // We also get the element as param on which transition is performed
+      console.log('beforeLeaveTransition');
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+button {
+  font: inherit;
+  padding: 0.5rem 2rem;
+  border: 1px solid #810032;
+  border-radius: 30px;
+  background-color: #810032;
+  color: white;
+  cursor: pointer;
+}
+button:hover,
+button:active {
+  background-color: #a80b48;
+  border-color: #a80b48;
+}
+.block {
+  width: 8rem;
+  height: 8rem;
+  background-color: #290033;
+  margin-bottom: 2rem;
+  /* transition: CSS-property-name duration function name; */
+  /* transition: transform 0.3s ease-out; */
+}
+.container {
+  max-width: 40rem;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem;
+  border: 2px solid #ccc;
+  border-radius: 12px;
+}
+.animate {
+  /* Moves object by 50px to left */
+  /* transform: translateX(-150px); */
+  /* custom-fade - Name of keyframe 
+    0.3s duration
+    ease-out - animation
+    forwards - Keep final keey frame
+  */
+
+  animation: custom-fade 0.3s ease-out forwards;
+}
+@keyframes custom-fade {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+  70% {
+    transform: translateX(-120px) scale(1.2);
+  }
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
+}
+/* ******************* Paragraph events  ************* */
+/* Enter */
+
+.para-enter-active {
+  /* All changes occuring wil be in duration of 0.3s with eas-out animation  */
+  /* transition: all 0.3s ease-out; */
+  animation: custom-fade 0.3s ease-out forwards;
+}
+
+/* Leave */
+.para-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+.para-leave-active {
+  /* All changes occuring wil be in duration of 0.3s with eas-out animation  */
+  transition: all 0.3s ease-in;
+}
+.para-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+/* Button animation */
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
+}
+</style>
