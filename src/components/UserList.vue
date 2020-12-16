91955be8f4356cc6fc073e0aa5d2391bf4798f9a
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-16 18:50:31 +0000

    Added list animation

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,11 +1,11 @@
 <template>
-  <ul>
-    <transition-group>
-      <li v-for="(user, index) in users" :key="index" @click="removeUser(user)">
-        {{ user }}
-      </li>
-    </transition-group>
-  </ul>
+  <!-- <ul> -->
+  <transition-group tag="ul" name="user-list">
+    <li v-for="user in users" :key="user" @click="removeUser(user)">
+      {{ user }}
+    </li>
+  </transition-group>
+  <!-- </ul> -->
   <div>
     <input type="text" ref="userName" />
     <button @click="addUser">Add User</button>
@@ -38,5 +38,25 @@ li {
   border: 1px solid #ccc;
   padding: 1rem;
   text-align: center;
+}
+.user-list-enter-from {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+.user-list-enter-to,
+.user-list-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+}
+.user-list-enter-active {
+  transition: all 1s ease-out;
+}
+
+.user-list-leave-active {
+  transition: all 1s ease-in;
+}
+.user-list-leave-to {
+  opacity: 1;
+  transform: translateX(30px);
 }
 </style>
