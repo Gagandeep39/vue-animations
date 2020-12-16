5761bd51fa297dbde433c6a901a34bec0befb395
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-16 13:02:05 +0000

    Added dummy code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
 
-createApp(App).use(router).mount('#app')
+import App from './App.vue';
+import BaseModal from './components/BaseModal.vue';
+
+createApp(App)
+  .component('base-modal', BaseModal)
+  .mount('#app');
